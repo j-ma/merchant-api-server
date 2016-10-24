8c62b42e3eaeaa2bbeeeceb74c86047a4d6e4df1
j-ma <jma2572@gmail.com>
2016-10-24 14:00:52 -0700

    [routes] Added get for recent-transactions

--- routes.js
+++ routes.js
@@ -11,6 +11,24 @@ REST_ROUTER.prototype.handleRoutes = function(router,connection) {
         res.json({"Message" : "Hello World !"});
     });
 
+    router.get("/recent-transactions",function(req,res) {
+    	var request = new mssql.Request(connection);
+
+    	request.query(`SELECT TOP 10 WebPointOrderHead.OrderTime, WebPointOrderHead.SoldAmount, 
+    			WebPointOrderHead.Points, WebPointOrderHead.Reference, WebCustomer.FirstName, WebCustomer.LasName,  
+    			WebPointOrderHead.PointCardCode, WebPointOrderHead.Remark
+			FROM WebPointOrderHead
+			INNER JOIN WebCustomer
+			ON WebPointOrderHead.CustomerID=WebCustomer.CustomerID
+			ORDER BY WebPointOrderHead.OrderTime DESC`, function(err, rows) {
+    		if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json(rows);
+            }
+    	});
+    });
+
     router.get("/transactions",function(req,res) {
     	var request = new mssql.Request(connection);
 
@@ -19,11 +37,12 @@ REST_ROUTER.prototype.handleRoutes = function(router,connection) {
     			WebPointOrderHead.PointCardCode, WebPointOrderHead.Remark
 			FROM WebPointOrderHead
 			INNER JOIN WebCustomer
-			ON WebPointOrderHead.CustomerID=WebCustomer.CustomerID`, function(err, rows) {
+			ON WebPointOrderHead.CustomerID=WebCustomer.CustomerID
+			ORDER BY WebPointOrderHead.OrderTime DESC`, function(err, rows) {
     		if(err) {
                 res.json({"Error" : true, "Message" : "Error executing MySQL query"});
             } else {
-                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+                res.json(rows);
             }
     	});
     });
